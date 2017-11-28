47ccc36456881166a7c84639bc9ace5e6393e2dd
raed.abdennad <raed.abdennadher@etu.hesge.ch>
2017-11-28 16:30:16 +0100

    update vagrant file and add a shell script to change the network adapter to bridge

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -5,7 +5,7 @@ cp /vagrant/yum.conf /etc/yum.conf
 mv /etc/sysconfig/network-scripts/ifcfg-eth0 /etc/sysconfig/network-scripts/ifcfg-eth0_old
 cp /vagrant/ifcfg-eth0 /etc/sysconfig/network-scripts/ifcfg-eth0
 
-sudo service network restart
+#sudo systemctl restart
 
 # apt-get update
 # apt-get install -y apache2
