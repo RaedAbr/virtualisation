c2fb92fd15c057dce9601c1e37b761c7dd643d82
steven.liatti <steven.liatti@etu.hesge.ch>
2017-12-27 09:38:41 +0100

    Try to connect to centos in SSH
    On branch master
    
    Changes to be committed:
    	new file:   vagrant/.gitignore
    	modified:   vagrant/bootstrap.sh
    	new file:   vagrant/id_rsa.pub
    	new file:   vagrant/sshd_config

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -8,3 +8,8 @@ mv /etc/sysconfig/network-scripts/ifcfg-eth0 /etc/sysconfig/network-scripts/ifcf
 cp /vagrant/ifcfg-eth0 /etc/sysconfig/network-scripts/ifcfg-eth0
 
 sudo yum update -y
+
+sudo mv /etc/ssh/sshd_config /etc/ssh/sshd_config_old
+sudo cp /vagrant/sshd_config /etc/ssh/sshd_config
+
+#cp /vagrant/id_rsa.pub /home/vagrant/.ssh/authorized_keys
